dffee53155c60bd72a377bbdf716467d4579338a
hbehera27 <harishb2727@gmail.com>
2025-04-29 20:21:48 +0530

    some correction

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header />
       <main>
-        <Meals />
+        <MealsSummary />
       </main>
     </Fragment>
   );
